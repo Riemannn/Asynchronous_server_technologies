8f647ea6bb1cd52742d276472e8dfc5a112cbbe5
Marie <perin.mariemp>
2017-10-09 10:48:19 +0200

    re init

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ var http = require('http');
 var server = require('./server.js');
 
 // Declare an HTTP server
-http.createServer(server.logic).listen(8888, '127.0.0.1');
+http.createServer(server.logic).listen(server.port, server.address);
